335e449b26f8de3428a1da5e55c1b6a105d67251
Jhonatan Stiven Rios Villa <1143869016@academico.icesi.edu.co>
2018-05-25 11:18:13 -0500

    Organizar Archivos y parte grafica

--- parcial.js
+++ parcial.js
@@ -13,3 +13,24 @@ app.use(express.static('public'));
 // Iniciar servidor
 app.listen(5000);
 console.log("Escuchando servidor")
+
+app.get('/', (req, res) => {
+    res.render('inicio', {
+        tittle: "Parcial Final",
+        nombre: "Inicio"
+    });
+})
+
+app.get('/proyec', (req, res) => {
+    res.render('proyecto', {
+        tittle: "Parcial Final",
+        nombre: "Proyectos"
+    });
+})
+
+app.get('/contac', (req, res) => {
+    res.render('contacto', {
+        tittle: "Parcial Final",
+        nombre: "Contacto"
+    });
+})
