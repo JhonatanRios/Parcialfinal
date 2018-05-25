fabe97f43e81566b4e7263d529f1eba4adf00d8f
Jhonatan Stiven Rios Villa <1143869016@academico.icesi.edu.co>
2018-05-25 12:00:52 -0500

    Tratar de hacer las Sumas

--- parcial.js
+++ parcial.js
@@ -1,6 +1,10 @@
 const express = require('express'),
-    engines = require('consolidate');
-fs = require('fs');
+    engines = require('consolidate'),
+    fs = require('fs');
+
+var inicio = 0;
+var proyecto = 0;
+var contact = 0;
 
 var app = express();
 
@@ -16,15 +20,18 @@ app.listen(5000);
 console.log("Escuchando servidor")
 
 app.get('/', (req, res) => {
+    var inicio =+ 1;
+    var proyecto = proyecto;
+    var contact = contact;
 
-    var inicio = inicio + 1;
-
-    fs.writeFile('./visitas.txt', '{\n"inicio": ' + inicio + ';\n"proyecto": 0;\n"contacto": 0;\n}', error => {
+    fs.writeFile('./visitas.txt', '{\n"inicio": ' + inicio + ';\n"proyecto": ' + proyecto + ';\n"contacto": ' + contact + ';\n}', error => {
         if (error)
             console.log(error);
         else
             console.log('El archivo fue creado');
+        console.log(inicio);
     });
+
     res.render('inicio', {
         tittle: "Parcial Final",
         nombre: "Inicio"
@@ -32,6 +39,18 @@ app.get('/', (req, res) => {
 })
 
 app.get('/proyec', (req, res) => {
+    var inicio = inicio;
+    var proyecto =+ 1;
+    var contact = contact;
+
+    fs.writeFile('./visitas.txt', '{\n"inicio": ' + inicio + ';\n"proyecto": ' + proyecto + ';\n"contacto": ' + contact + ';\n}', error => {
+        if (error)
+            console.log(error);
+        else
+            console.log('El archivo fue creado');
+        console.log(inicio);
+    });
+
     res.render('proyecto', {
         tittle: "Parcial Final",
         nombre: "Proyectos"
@@ -39,8 +58,28 @@ app.get('/proyec', (req, res) => {
 })
 
 app.get('/contac', (req, res) => {
+    var inicio = inicio;
+    var proyecto = proyecto;
+    var contact =+1;
+
+    fs.writeFile('./visitas.txt', '{\n"inicio": ' + inicio + ';\n"proyecto": ' + proyecto + ';\n"contacto": ' + contact + ';\n}', error => {
+        if (error)
+            console.log(error);
+        else
+            console.log('El archivo fue creado');
+        console.log(inicio);
+    });
+
     res.render('contacto', {
         tittle: "Parcial Final",
         nombre: "Contacto"
     });
 })
+
+fs.readFile('visitas.txt', 'utf-8', (err, data) => {
+    if(err) {
+      console.log('error: ', err);
+    } else {
+      console.log(data);
+    }
+  });
